76970f9e78f78a47facdb58e148b40ff2626f8bd
French Michael Leyran <141644085+leyrankel@users.noreply.github.com>
2024-01-13 11:19:50 +0800

    SimpleCalculator.java

---
+++ java/SimpleCalculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+ 
+public class SimpleCalculator {
+    public static void main(String[] args)
+    {
+        Scanner kel = new Scanner(System.in);
+ 
+
+        System.out.print("Enter the first number: ");
+        int firstNumber = kel.nextInt();
+        System.out.print("Enter the second number: ");
+        int secondNumber = kel.nextInt();
+ 
+
+        System.out.print("Enter the type of operation you want to perform (+, -, *, /, %): ");
+        String operation = kel.next();
+        int result = performOperation(firstNumber, secondNumber, operation);
+        System.out.println("Your answer is: " + result);
+    }
+ 
+    public static int performOperation(int firstNumber, int secondNumber, String operation)
+    {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "%":
+                result = firstNumber % secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                System.out.println("Invalid operation");
+                break;
+        }
+        return result;
+    }
+}
